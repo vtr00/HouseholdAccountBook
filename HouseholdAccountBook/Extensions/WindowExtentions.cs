3c5888d75011fde6a5a5082b710f28f099f44a49
toresebu <vtr0749@gmail.com>
2024-01-28 14:51:57 +0900

    表示に関する不具合の修正

--- HouseholdAccountBook/Extensions/WindowExtentions.cs
+++ HouseholdAccountBook/Extensions/WindowExtentions.cs
@@ -15,16 +15,22 @@ namespace HouseholdAccountBook.Extensions
         public static void MoveOwnersCenter(this Window window)
         {
             if (window.Owner != null) {
-                window.Left = window.Owner.Left + (window.Owner.Width - window.Width) / 2;
-                window.Top = window.Owner.Top + (window.Owner.Height - window.Height) / 2;
+                if (window.Owner.WindowState == WindowState.Normal) {
+                    window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                    //window.Left = window.Owner.Left + (window.Owner.Width - window.Width) / 2;
+                    //window.Top = window.Owner.Top + (window.Owner.Height - window.Height) / 2;
+                }
+                else {
+                    window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                }
 
                 double right = window.Left + window.Width;
                 double bottom = window.Top + window.Height;
                 double OwnerRight = window.Owner.Left + window.Owner.Width;
                 double OwnerBottom = window.Owner.Left + window.Owner.Height;
 
-                Debug.WriteLine(string.Format("window: top {0}, right {1}, bottom {2}, left {3}, width {4}, height {5}", window.Top, right, bottom, window.Left, window.Width, window.Height));
-                Debug.WriteLine(string.Format("owner : top {0}, right {1}, bottom {2}, left {3}, width {4}, height {5}", window.Owner.Top, OwnerRight, OwnerBottom, window.Owner.Left, window.Owner.Width, window.Owner.Height));
+                Log.Info(string.Format("window - top:{0} right:{1} bottom:{2} left:{3} width:{4} height:{5}", window.Top, right, bottom, window.Left, window.Width, window.Height));
+                Log.Info(string.Format("owner  - top:{0} right:{1} bottom:{2} left:{3} width:{4} height:{5}", window.Owner.Top, OwnerRight, OwnerBottom, window.Owner.Left, window.Owner.Width, window.Owner.Height));
             }
         }
     }
