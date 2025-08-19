a9aeca09314f6b465b18af30cb9a05301be33119
toresebu <vtr0749@gmail.com>
2025-08-20 01:02:39 +0900

    パス画からの場合に例外が発生するのを修正

--- HouseholdAccountBook/Extensions/PathExtensions.cs
+++ HouseholdAccountBook/Extensions/PathExtensions.cs
@@ -8,18 +8,31 @@ namespace HouseholdAccountBook.Extensions
         /// <summary>
         /// 基準とするフォルダからの相対パスを取得する
         /// </summary>
-        /// <param name="basePath">基準とするフォルダパス</param>
+        /// <param name="fullRootPath">相対パスの基準とするフォルダパス</param>
         /// <param name="targetPath">相対パスを取得したいファイル/フォルダパス</param>
-        /// <returns>basePath から targetPath への相対パス。targetPath が basePath 配下にない場合は targetPath のフルパス</returns>
-        public static string GetSmartPath(string basePath, string targetPath)
+        /// <param name="fullBasePath">絶対パスの基準とするフォルダパス</param>
+        /// <returns>fullRootPath から targetPath への相対パス。targetPath が fullRootPath 配下にない場合は targetPath のフルパス</returns>
+        public static string GetSmartPath(string fullRootPath, string targetPath, string fullBasePath = "")
         {
-            // 絶対パスに変換
-            string fullBasePath = Path.GetFullPath(basePath);
-            string fullTargetPath = Path.GetFullPath(targetPath);
+            if (string.IsNullOrEmpty(targetPath)) {
+                return string.Empty;
+            }
+            if (!Path.IsPathFullyQualified(fullRootPath)) {
+                throw new Exception("fullRootPath must be an absolute path.");
+            }
+            if (string.IsNullOrEmpty(fullBasePath)) {
+                fullBasePath = fullRootPath;
+            }
+            if (!Path.IsPathFullyQualified(fullBasePath)) {
+                throw new Exception("fullBasePath must be an absolute path.");
+            }
 
-            // targetPath が basePath の下にあるか確認
-            if (fullTargetPath.StartsWith(fullBasePath, StringComparison.OrdinalIgnoreCase)) {
-                return Path.GetRelativePath(fullBasePath, fullTargetPath);
+            // 絶対パスに変換
+            string fullTargetPath = Path.GetFullPath(targetPath, fullBasePath);
+
+            // targetPath が fullRootPath の下にあるか確認
+            if (fullTargetPath.StartsWith(fullRootPath, StringComparison.OrdinalIgnoreCase)) {
+                return Path.GetRelativePath(fullRootPath, fullTargetPath);
             }
             else {
                 return fullTargetPath;
@@ -30,11 +43,19 @@ namespace HouseholdAccountBook.Extensions
         /// フォルダパスとファイル名/フォルダ名に分割する
         /// </summary>
         /// <param name="path">対象のファイル/フォルダパス</param>
+        /// <param name="fullBasePath">絶対パスの基準とするフォルダパス</param>
         /// <returns>フォルダパスとファイル名/フォルダ名のタプル</returns>
-        public static (string, string) GetSeparatedPath(string path, string basePath)
+        public static (string, string) GetSeparatedPath(string path, string fullBasePath)
         {
+            if (string.IsNullOrEmpty(path)) {
+                return (fullBasePath, string.Empty);
+            }
+            if (!Path.IsPathFullyQualified(fullBasePath)) {
+                throw new Exception("fullBasePath must be an absolute path.");
+            }
+
             // 絶対パスに変換
-            string fullPath = Path.GetFullPath(path, basePath);
+            string fullPath = Path.GetFullPath(path, fullBasePath);
 
             // パスを分割
             string directory = Path.GetDirectoryName(fullPath) ?? string.Empty;
