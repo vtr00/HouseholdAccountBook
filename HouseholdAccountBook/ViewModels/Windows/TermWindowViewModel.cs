32518cd81383459a7c957a0205ae7178b236b812
toresebu <vtr0749@gmail.com>
2025-09-12 00:24:07 +0900

    TermWindowをVVM化

--- HouseholdAccountBook/ViewModels/Windows/TermWindowViewModel.cs
+++ HouseholdAccountBook/ViewModels/Windows/TermWindowViewModel.cs
@@ -1,13 +1,20 @@
-﻿using HouseholdAccountBook.ViewModels.Abstract;
+﻿using HouseholdAccountBook.Extensions;
+using HouseholdAccountBook.Models.Dao.Compositions;
+using HouseholdAccountBook.Models.DbHandler.Abstract;
+using HouseholdAccountBook.ViewModels.Abstract;
 using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace HouseholdAccountBook.ViewModels.Windows
 {
     /// <summary>
     /// 期間指定ウィンドウVM
     /// </summary>
-    public class TermWindowViewModel : BindableBase
+    public class TermWindowViewModel : WindowViewModelBase
     {
+        #region Bindingプロパティ
         /// <summary>
         /// 開始日
         /// </summary>
@@ -31,5 +38,77 @@ namespace HouseholdAccountBook.ViewModels.Windows
         }
         private DateTime _EndDate = DateTime.Now;
         #endregion
+
+        /// <summary>
+        /// 今月ボタンクリック時のコマンド
+        /// </summary>
+        public ICommand ThisMonthCommand => new RelayCommand(this.ThisMonthCommand_Executed);
+        /// <summary>
+        /// 全期間ボタンクリック時のコマンド
+        /// </summary>
+        public ICommand AllTermCommand => new RelayCommand(this.AllTermCommand_Executed);
+        #endregion
+
+        /// <summary>
+        /// 今月ボタンクリック時のコマンド処理
+        /// </summary>
+        void ThisMonthCommand_Executed()
+        {
+            this.StartDate = DateTime.Today.GetFirstDateOfMonth();
+            this.EndDate = DateTime.Today.GetLastDateOfMonth();
+        }
+
+        /// <summary>
+        /// 全期間ボタンクリック時のコマンド処理
+        /// </summary>
+        async void AllTermCommand_Executed()
+        {
+            var firstLastPair = await this.LoadFirstLastDate();
+            this.StartDate = firstLastPair.Item1;
+            this.EndDate = firstLastPair.Item2;
+        }
+
+        #region ウィンドウ設定プロパティ
+        public override Rect WindowRectSetting
+        {
+            set {
+                Properties.Settings settings = Properties.Settings.Default;
+
+                if (settings.App_IsPositionSaved) {
+                    settings.TermWindow_Left = value.Left;
+                    settings.TermWindow_Top = value.Top;
+                }
+
+                settings.Save();
+            }
+        }
+
+        public override Size? WindowSizeSetting => null;
+
+        public override Point? WindowPointSetting
+        {
+            get {
+                Properties.Settings settings = Properties.Settings.Default;
+                return WindowPointSettingImpl(settings.TermWindow_Left, settings.TermWindow_Top, settings.App_IsPositionSaved);
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// 帳簿項目の初日/最終日を取得する
+        /// </summary>
+        /// <returns>初日/最終日のペア</returns>
+        private async Task<Tuple<DateTime, DateTime>> LoadFirstLastDate()
+        {
+            DateTime firstTime = DateTime.Today;
+            DateTime lastTime = DateTime.Today;
+            await using (DbHandlerBase dbHandler = await this.dbHandlerFactory.CreateAsync()) {
+                TermInfoDao termInfoDao = new(dbHandler);
+                var dto = await termInfoDao.Find();
+                firstTime = dto.FirstTime;
+                lastTime = dto.LastTime;
+            }
+            return new Tuple<DateTime, DateTime>(firstTime, lastTime);
+        }
     }
 }
--- HouseholdAccountBook/Views/Windows/TermWindow.xaml.cs
+++ HouseholdAccountBook/Views/Windows/TermWindow.xaml.cs
@@ -1,10 +1,6 @@
-﻿using HouseholdAccountBook.Enums;
-using HouseholdAccountBook.Extensions;
-using HouseholdAccountBook.Models.Dao.Compositions;
+﻿using HouseholdAccountBook.Extensions;
 using HouseholdAccountBook.Models.DbHandler;
-using HouseholdAccountBook.Models.DbHandler.Abstract;
 using System;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -16,145 +12,67 @@ namespace HouseholdAccountBook.Views.Windows
     /// </summary>
     public partial class TermWindow : Window
     {
-        #region フィールド
-        /// <summary>
-        /// DBハンドラファクトリ
-        /// </summary>
-        private readonly DbHandlerFactory dbHandlerFactory;
-        /// <summary>
-        /// 選択された開始日
-        /// </summary>
-        private readonly DateTime? selectedStartDate;
-        /// <summary>
-        /// 選択された終了日
-        /// </summary>
-        private readonly DateTime? selectedEndDate;
-        /// <summary>
-        /// 選択された期間種別
-        /// </summary>
-        private readonly TermKind selectedTermKind;
-        #endregion
-
         #region コンストラクタ
         /// <summary>
         /// <see cref="TermWindow"/> クラスの新しいインスタンスを初期化します。
         /// </summary>
+        /// <param name="owner">親ウィンドウ</param>
         /// <param name="dbHandlerFactory">DBハンドラファクトリ</param>
         /// <param name="dateWithinMonth">月内日付</param>
         public TermWindow(DbHandlerFactory dbHandlerFactory, DateTime dateWithinMonth)
         {
-            this.dbHandlerFactory = dbHandlerFactory;
-
             this.InitializeComponent();
 
-            this.selectedStartDate = dateWithinMonth.GetFirstDateOfMonth();
-            this.selectedEndDate = dateWithinMonth.GetLastDateOfMonth();
-            this.selectedTermKind = TermKind.Monthly;
+            this.RegisterEventHandlers(this.WVM);
+            // ロード時処理はコンストラクタで設定しておく
+            this.Loaded += (sender, e) => {
+                // xamlで指定するとCalendarが正しく表示されないため、ここで指定する
+                this.calendar.DisplayMode = CalendarMode.Year;
+
+                this.WVM.StartDate = dateWithinMonth.GetFirstDateOfMonth();
+                this.WVM.EndDate = dateWithinMonth.GetLastDateOfMonth();
+                this.selectedMonthRadioButton.IsChecked = true;
+            };
+
+            this.WVM.Initialize(dbHandlerFactory);
+
         }
 
         /// <summary>
         /// <see cref="TermWindow"/> クラスの新しいインスタンスを初期化します。
         /// </summary>
+        /// <param name="owner">親ウィンドウ</param>
         /// <param name="dbHandlerFactory">DBハンドラファクトリ</param>
         /// <param name="startDate">開始日</param>
         /// <param name="endDate">終了日</param>
         public TermWindow(DbHandlerFactory dbHandlerFactory, DateTime startDate, DateTime endDate)
         {
-            this.dbHandlerFactory = dbHandlerFactory;
-
             this.InitializeComponent();
 
-            this.selectedStartDate = startDate;
-            this.selectedEndDate = endDate;
-            this.selectedTermKind = TermKind.Selected;
+            this.RegisterEventHandlers(this.WVM);
+            // ロード時処理はコンストラクタで設定しておく
+            this.Loaded += (sender, e) => {
+                // xamlで指定するとCalendarが正しく表示されないため、ここで指定する
+                this.calendar.DisplayMode = CalendarMode.Year;
+
+                this.WVM.StartDate = startDate;
+                this.WVM.EndDate = endDate;
+                this.selectedTermRadioButton.IsChecked = true;
+            };
+
+            this.WVM.Initialize(dbHandlerFactory);
+
         }
         #endregion
 
         #region イベントハンドラ
-        #region コマンド
-        /// <summary>
-        /// 指定月選択時
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void CheckSelectedMonthCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            // NOP
-            // ウィンドウ終了時に開始/終了日を指定する
-        }
-
-        /// <summary>
-        /// 今月選択時
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void ThisMonthCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            this.WVM.StartDate = DateTime.Today.GetFirstDateOfMonth();
-            this.WVM.EndDate = DateTime.Today.GetLastDateOfMonth();
-        }
-
-        /// <summary>
-        /// 指定期間選択時
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void CheckSelectedTermCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            // NOP
-        }
-
-        /// <summary>
-        /// 全期間選択時
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private async void AllTermCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            Tuple<DateTime, DateTime> firstLastDate = await this.LoadFirstLastDate();
-            this.WVM.StartDate = firstLastDate.Item1;
-            this.WVM.EndDate = firstLastDate.Item2;
-        }
-        #endregion
-
         #region ウィンドウ
-        /// <summary>
-        /// ウィンドウ読込完了時
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void TermWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            // xamlで指定するとCalendarが正しく表示されないため、ここで指定する
-            this.calendar.DisplayMode = CalendarMode.Year;
-
-            switch (this.selectedTermKind) {
-                case TermKind.Monthly:
-                    this.WVM.StartDate = this.selectedStartDate.Value;
-                    this.WVM.EndDate = this.selectedEndDate.Value;
-                    this.selectedMonthRadioButton.IsChecked = true;
-                    break;
-                case TermKind.Selected:
-                    this.WVM.StartDate = this.selectedStartDate.Value;
-                    this.WVM.EndDate = this.selectedEndDate.Value;
-                    this.selectedTermRadioButton.IsChecked = true;
-                    break;
-            }
-        }
-
-        /// <summary>
-        /// ウィンドウ終了時
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void TermWindow_Closed(object sender, EventArgs e)
         {
             if (this.selectedMonthRadioButton.IsChecked.Value) {
                 this.WVM.StartDate = this.WVM.StartDate.GetFirstDateOfMonth();
                 this.WVM.EndDate = this.WVM.StartDate.GetLastDateOfMonth();
             }
-
-            this.SaveWindowSetting();
         }
         #endregion
 
@@ -173,77 +91,6 @@ namespace HouseholdAccountBook.Views.Windows
                 _ = Mouse.Capture(null);
             }
         }
-
-        /// <summary>
-        /// OKボタン押下時
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void OKCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            this.DialogResult = true;
-            this.Close();
-        }
-
-        /// <summary>
-        /// キャンセルボタン押下時
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void CancelCommand_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            this.DialogResult = false;
-            this.Close();
-        }
         #endregion
-
-        #region 設定反映用の関数
-        /// <summary>
-        /// ウィンドウ設定を読み込む
-        /// </summary>
-        public void LoadWindowSetting()
-        {
-            Properties.Settings settings = Properties.Settings.Default;
-
-            if (settings.App_IsPositionSaved && 0 <= settings.TermWindow_Left && 0 <= settings.TermWindow_Top) {
-                this.Left = settings.TermWindow_Left;
-                this.Top = settings.TermWindow_Top;
-            }
-            else {
-                this.MoveOwnersCenter();
-            }
-        }
-
-        /// <summary>
-        /// ウィンドウ設定を保存する
-        /// </summary>
-        public void SaveWindowSetting()
-        {
-            Properties.Settings settings = Properties.Settings.Default;
-
-            if (settings.App_IsPositionSaved) {
-                settings.TermWindow_Left = this.Left;
-                settings.TermWindow_Top = this.Top;
-            }
-            settings.Save();
-        }
-        #endregion
-
-        /// <summary>
-        /// 帳簿項目の初日/最終日を取得する
-        /// </summary>
-        /// <returns>初日/最終日のペア</returns>
-        private async Task<Tuple<DateTime, DateTime>> LoadFirstLastDate()
-        {
-            DateTime firstTime = DateTime.Today;
-            DateTime lastTime = DateTime.Today;
-            await using (DbHandlerBase dbHandler = await this.dbHandlerFactory.CreateAsync()) {
-                TermInfoDao termInfoDao = new(dbHandler);
-                var dto = await termInfoDao.Find();
-                firstTime = dto.FirstTime;
-                lastTime = dto.LastTime;
-            }
-            return new Tuple<DateTime, DateTime>(firstTime, lastTime);
-        }
     }
 }
