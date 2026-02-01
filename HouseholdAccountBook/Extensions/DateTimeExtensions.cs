17185866c493189e1b89aa386e21d31b9a56ec1a
toresebu <vtr0749@gmail.com>
2026-02-02 00:05:37 +0900

    #52 汎用的な処理を設定非依存に変更する
    - DateTimeExtensionsからDateTime非依存/設定依存関数を分離

--- HouseholdAccountBook/Extensions/DateTimeExtensions.cs
+++ HouseholdAccountBook/Extensions/DateTimeExtensions.cs
@@ -183,24 +183,5 @@ namespace HouseholdAccountBook.Extensions
             return !(startDateTime1 != null && endDateTime2 != null && (endDateTime2 < startDateTime1))
                 && !(startDateTime2 != null && endDateTime1 != null && (endDateTime1 < startDateTime2));
         }
-
-        /// <summary>
-        /// 設定に応じた年の単位を取得する
-        /// </summary>
-        /// <param name="startMonth">会計年度開始月</param>
-        /// <returns>年の単位</returns>
-        public static string GetYearUnit(int startMonth) => startMonth == 1 ? Properties.Resources.Unit_Year : Properties.Resources.Unit_FiscalYear;
-        /// <summary>
-        /// 設定に応じた年の単位(前置)を取得する
-        /// </summary>
-        /// <param name="startMonth">会計年度開始月</param>
-        /// <returns></returns>
-        public static string GetYearPreUnit(int startMonth) => startMonth == 1 ? string.Empty : Properties.Resources.Unit_FiscalYear_Pre;
-        /// <summary>
-        /// 設定に応じた年の単位(後置)を取得する
-        /// </summary>
-        /// <param name="startMonth">会計年度開始月</param>
-        /// <returns></returns>
-        public static string GetYearPostUnit(int startMonth) => startMonth == 1 ? string.Empty : Properties.Resources.Unit_FiscalYear_Post;
     }
 }
